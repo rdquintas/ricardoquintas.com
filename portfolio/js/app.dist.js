a737f5e9cf35d773ec9590260bec1f238b61037c
Ricardo Quintas <rqcenter-web@yahoo.com>
2015-12-09 22:20:07 +0100

    portfolio

--- portfolio/js/app.dist.js
+++ portfolio/js/app.dist.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.slidebars();
-    $( "#nav-menu ul" ).clone().appendTo( ".sb-slidebar" );
+    $("#nav-menu ul").clone().appendTo(".sb-slidebar");
 });
 
 // Handlebars
@@ -8,6 +8,39 @@ Handlebars.registerHelper('skills_class', function() {
     var str = this.skills.join(" ");
     return str;
 });
+
+// Handlebars
+Handlebars.registerHelper('get_skill_css_color', function(skill) {
+
+//     #61bd4f     green
+// #f2d600     yellow
+// #ffab4a     orange
+// #eb5a46     red
+// #c377e0     purple
+// #0079bf     blue
+// #00c2e0     light-blue
+// #51e898     light-green
+
+
+    switch (skill) {
+        case "LESS":
+            return "blue";
+            break;
+        case "NodeJS":
+            return "green";
+            break;
+        case "PHP":
+            return "purple";
+            break;
+        case "Bootstrap":
+            return "light-blue";
+            break;
+        default:
+            return "grey";
+            break;
+    }
+});
+
 
 
 var source = $("#entry-template").html();
@@ -25,7 +58,7 @@ $('#entry-template').imagesLoaded(function() {
     });
 
 
-    $('#nav-menu ul li').on("click", function() {       
+    $('#nav-menu ul li').on("click", function() {
         setActive(this);
         var skill = $(this).attr("data-skill");
 
