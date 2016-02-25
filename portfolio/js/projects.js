ed25752fab0c7b84e95d5eb7f4f77d0ad7d4758f
rdquintas <rqcenter-web@yahoo.com>
2016-02-25 23:07:34 +0000

    changes to portfolio

--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -1,97 +1,122 @@
 this.projects = [{
-    name: "Lisbon Swingers",
-    description: "Big band Jazz Orchestra. <br> This responsive website required complex UI/UX components and subtle animations. <br>All done with plain and pure CSS, no framework.",
-    image: "lisbonswingers.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://www.lisbonswingers.com/index_en.html",
-        github: "https://github.com/rdquintas/LisbonSwingers",
-        simple: "lisbonswingers.com",
-        liveSite: "http://www.lisbonswingers.com/index_en.html"
-    },
-    skills: ["jQuery", "LESS"]
+  name: 'Lisbon Swingers',
+  description: 'Big band Jazz Orchestra. <br> This responsive website required complex UI/UX components and subtle animations. <br>All done with plain and pure CSS, no framework.',
+  image: 'lisbonswingers.jpg',
+  url: {
+    preview: 'http://www.lisbonswingers.com/index_en.html',
+    github: 'https://github.com/rdquintas/LisbonSwingers',
+    simple: 'lisbonswingers.com',
+    liveSite: 'http://www.lisbonswingers.com/index_en.html'
+  },
+  skills: ['Javascript', 'LESS']
 }, {
-    name: "Unplace Museum",
-    description: "This project was for one of the biggest Modern Art institutions in Portugal (Fundação Calouste Gulbenkian).<br> The project was for a temporary international exhibition of world-wide modern artists. Site required inovative design and simple CMS support. <br>Extensive use of plain vanilla Javascript.",
-    image: "unplace.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://www.ricardoquintas.com/portfolio/unplace/#%7B%22language%22%3A%22en%22%2C%22project_id%22%3Anull%2C%22tour_id%22%3Anull%7D",
-        github: "https://github.com/rdquintas/unplace.org",
-        simple: "unplace.org",
-        liveSite: "http://www.unplace.org"
-    },
-    skills: ["jQuery", "LESS"]
+  name: 'London Node Users Group',
+  description: 'I contribute and colaborate fixing open issues of LNUG website.',
+  image: 'lnug.jpg',
+  url: {
+    preview: 'http://www.lnug.org',
+    github: 'https://github.com/rdquintas/lnug.github.io',
+    simple: 'lnug.org',
+    liveSite: 'http://www.lnug.org'
+  },
+  skills: ['Javascript', 'NodeJS']
 }, {
-    name: "Santinho",
-    description: "Proof of concept for a religious institution. <br>Features inovative UI/UX components.",
-    image: "santinho.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://www.ricardoquintas.com/portfolio/santinho",
-        github: "https://github.com/rdquintas/santinho",
-        simple: "",
-        liveSite: ""
-    },
-    skills: ["jQuery"]
+  name: 'Unplace Museum',
+  description: 'This project was for one of the biggest Modern Art institutions in Portugal (Fundação Calouste Gulbenkian).<br> The project was for a temporary international exhibition of world-wide modern artists. Site required inovative design and simple CMS support. <br>Extensive use of plain vanilla Javascript.',
+  image: 'unplace.jpg',
+  url: {
+    preview: 'http://www.ricardoquintas.com/portfolio/unplace/#%7B%22language%22%3A%22en%22%2C%22project_id%22%3Anull%2C%22tour_id%22%3Anull%7D',
+    github: 'https://github.com/rdquintas/unplace.org',
+    simple: 'unplace.org',
+    liveSite: 'http://www.unplace.org'
+  },
+  skills: ['Javascript', 'LESS']
 }, {
-    name: "Alexandre Camarão",
-    description: "Web portfolio of Portguese modern art artist Alexandre Camarão.<br> This website uses the flat file CMS framework: GRAV.",
-    image: "alexandrecamarao.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://www.ricardoquintas.com/portfolio/alexandre_camarao",
-        github: "https://github.com/rdquintas/alexandrecamarao.com",
-        simple: "alexandrecamarao.com",
-        liveSite: "http://www.alexandrecamarao.com"
-    },
-    skills: ["jQuery", "PHP", "LESS"]
+  name: 'Santinho',
+  description: 'Proof of concept for a religious institution. <br>Features inovative UI/UX components.',
+  image: 'santinho.jpg',
+  url: {
+    preview: 'http://www.ricardoquintas.com/portfolio/santinho',
+    github: 'https://github.com/rdquintas/santinho',
+    simple: '',
+    liveSite: ''
+  },
+  skills: ['Javascript']
 }, {
-    name: "AFSO",
-    description: "Website for Portguese ONG, AFSO. <br>This website uses the flat file CMS framework: GRAV.",
-    image: "afso.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://www.ricardoquintas.com/portfolio/afso",
-        github: "https://github.com/rdquintas/afso.pt",
-        simple: "afso.pt",
-        liveSite: "http://www.afso.pt"
-    },
-    skills: ["jQuery", "PHP", "LESS"]
+  name: 'Alexandre Camarão',
+  description: 'Web portfolio of Portguese modern art artist Alexandre Camarão.<br> This website uses the flat file CMS framework: GRAV.',
+  image: 'alexandrecamarao.jpg',
+  url: {
+    preview: 'http://www.alexandrecamarao.com',
+    github: 'https://github.com/rdquintas/alexandrecamarao.com',
+    simple: 'alexandrecamarao.com',
+    liveSite: 'http://www.alexandrecamarao.com'
+  },
+  skills: ['Javascript', 'PHP', 'LESS']
 }, {
-    name: "Alphalink",
-    description: "Civil Engineer company.<br> A simple website to display the company portfolio.<br> This website uses the flat file CMS framework: GRAV.",
-    image: "alphalink.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://ricardoquintas.com/portfolio/alphalink/en",
-        github: "https://github.com/rdquintas/Alphalink",
-        simple: "alphalink.pt",
-        liveSite: "http://www.alphalink.pt"
-    },
-    skills: ["jQuery", "PHP", "LESS"]
+  name: 'AFSO',
+  description: 'Website for Portguese ONG, AFSO. <br>This website uses the flat file CMS framework: GRAV.',
+  image: 'afso.jpg',
+  url: {
+    preview: 'http://www.ricardoquintas.com/portfolio/afso',
+    github: 'https://github.com/rdquintas/afso.pt',
+    simple: 'afso.pt',
+    liveSite: 'http://www.afso.pt'
+  },
+  skills: ['Javascript', 'PHP', 'LESS']
 }, {
-    name: "Ricardo Quintas",
-    description: "My own website. <br>This responsive single-page website features some simple animations.",
-    image: "ricardoquintas.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://ricardoquintas.com/",
-        github: "https://github.com/rdquintas/ricardoquintas.com",
-        simple: "ricardoquintas.com",
-        liveSite: "http://www.ricardoquintas.com"
-    },
-    skills: ["jQuery", "LESS", "Bootstrap"]
+  name: 'Alphalink',
+  description: 'Civil Engineer company.<br> A simple website to display the company portfolio.<br> This website uses the flat file CMS framework: GRAV.',
+  image: 'alphalink.jpg',
+  url: {
+    preview: 'http://ricardoquintas.com/portfolio/alphalink/en',
+    github: 'https://github.com/rdquintas/Alphalink',
+    simple: 'alphalink.pt',
+    liveSite: 'http://www.alphalink.pt'
+  },
+  skills: ['Javascript', 'PHP', 'LESS']
 }, {
-    name: "SCA",
-    description: "Proof of concept for SCA, a customer I've worked for. They requested me a POC for them to show to their stake-holders. <br>Very simple Bootstrap prototype.",
-    image: "sca.jpg",
-    // image: "300x200.png",
-    url: {
-        preview: "http://ricardoquintas.com/portfolio/sca",
-        github: "https://github.com/rdquintas/scademo",
-        simple: "sca.com",
-        liveSite: "http://www.sca.com/"
-    },
-    skills: ["jQuery", "Bootstrap"]
-}];
+  name: 'Ricardo Quintas',
+  description: 'My own website. <br>This responsive single-page website features some simple animations.',
+  image: 'ricardoquintas.jpg',
+  url: {
+    preview: 'http://ricardoquintas.com/',
+    github: 'https://github.com/rdquintas/ricardoquintas.com',
+    simple: 'ricardoquintas.com',
+    liveSite: 'http://www.ricardoquintas.com'
+  },
+  skills: ['Javascript', 'LESS', 'Bootstrap']
+}, {
+  name: 'SCA',
+  description: "Proof of concept for SCA, a customer I've worked for. They requested me a POC for them to show to their stake-holders. <br>Very simple Bootstrap prototype.",
+  image: 'sca.jpg',
+  url: {
+    preview: 'http://ricardoquintas.com/portfolio/sca',
+    github: 'https://github.com/rdquintas/scademo',
+    simple: 'sca.com',
+    liveSite: 'http://www.sca.com/'
+  },
+  skills: ['Javascript', 'Bootstrap']
+}, {
+  name: 'Jazz Quotes',
+  description: 'This was a prototype I did for a design challenge.',
+  image: 'jazzquotes.jpg',
+  url: {
+    preview: 'http://codepen.io/rdquintas/full/RabBQr/',
+    github: 'http://codepen.io/rdquintas/pen/RabBQr/',
+    simple: '',
+    liveSite: 'http://codepen.io/rdquintas/full/RabBQr/'
+  },
+  skills: ['Javascript']
+}, {
+  name: 'Cristina Belo',
+  description: 'Website of yoga guru Cristina Belo.',
+  image: 'cristinabelo.jpg',
+  url: {
+    preview: 'http://www.cristinabelo.com',
+    github: 'https://github.com/rdquintas/cristinabelo.com',
+    simple: 'cristinabelo.com',
+    liveSite: 'http://www.cristinabelo.com'
+  },
+  skills: ['Javascript']
+}]
