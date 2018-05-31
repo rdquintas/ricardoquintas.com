d8bde5669ad2b01f8033d8223df4474a1f862b9b
rdquintas <rqcenter-web@yahoo.com>
2018-05-31 16:49:44 +0100

    Updating home page and portfolio page

--- portfolio/js/app.dist.js
+++ portfolio/js/app.dist.js
@@ -1,48 +1,48 @@
-$(document).ready(function () {
-  var $grid
-  $.slidebars()
-  $('#nav-menu ul').clone().appendTo('.sb-slidebar')
-  $('#footer .year').html(new Date().getFullYear())
-  checkAllImagesAreLoaded()
+$(document).ready(function() {
+    var $grid
+    $.slidebars()
+    $('#nav-menu ul').clone().appendTo('.sb-slidebar')
+    $('#footer .year').html(new Date().getFullYear())
+    checkAllImagesAreLoaded()
 })
 
 // Handlebars get the skill CSS class
-Handlebars.registerHelper('skills_class', function () {
-  var str = this.skills.join(' ')
-  return str
+Handlebars.registerHelper('skills_class', function() {
+    var str = this.skills.join(' ')
+    return str
 })
 
 // Handlebars get the CSS color for the skill tag
-Handlebars.registerHelper('get_skill_css_color', function (skill) {
-  switch (skill) {
-    case 'LESS':
-      return 'blue'
-      break
-    case 'NodeJS':
-      return 'green'
-      break
-    case 'PHP':
-      return 'purple'
-      break
-    case 'Bootstrap':
-      return 'light-blue'
-      break
-    case 'Javascript':
-      return 'yellow'
-      break
-    case '????????':
-      return 'orange'
-      break
-    case '????????':
-      return 'red'
-      break
-    case '????????':
-      return 'light-green'
-      break
-    default:
-      return 'grey'
-      break
-  }
+Handlebars.registerHelper('get_skill_css_color', function(skill) {
+    switch (skill) {
+        case 'LESS':
+            return 'blue'
+            break
+        case 'NodeJS':
+            return 'green'
+            break
+        case 'PHP':
+            return 'purple'
+            break
+        case 'Bootstrap':
+            return 'light-blue'
+            break
+        case 'Javascript':
+            return 'yellow'
+            break
+        case '????????':
+            return 'orange'
+            break
+        case 'Angular':
+            return 'red'
+            break
+        case 'JAVA':
+            return 'light-green'
+            break
+        default:
+            return 'grey'
+            break
+    }
 })
 
 // Initialize Handlebars
@@ -51,58 +51,58 @@ var template = Handlebars.compile(source)
 var html = template(this.projects)
 $('#projects').html(html)
 
-function setActive (skill) {
-  $('#nav-menu ul li').removeClass('active')
-  $('#nav-menu-mobile ul li').removeClass('active')
-  $('#nav-menu ul li.' + skill).addClass('active')
-  $('#nav-menu-mobile ul li.' + skill).addClass('active')
+function setActive(skill) {
+    $('#nav-menu ul li').removeClass('active')
+    $('#nav-menu-mobile ul li').removeClass('active')
+    $('#nav-menu ul li.' + skill).addClass('active')
+    $('#nav-menu-mobile ul li.' + skill).addClass('active')
 }
 
-function initializeGrid () {
-  $grid = $('#projects')
-  $grid.isotope({
-    itemSelector: '.project-item',
-    layoutMode: 'fitRows'
-  })
+function initializeGrid() {
+    $grid = $('#projects')
+    $grid.isotope({
+        itemSelector: '.project-item',
+        layoutMode: 'fitRows'
+    })
 
-  // Capture skill-filter click events
-  $('ul.menu li').on('click', function () {
-    var skill = $(this).attr('data-skill')
-    if (skill === 'Home') {
-      return
-    }
-    setActive(skill)
-    $.slidebars.close()
-    if (skill === 'All') {
-      $grid.isotope({
-        filter: '*'
-      })
-    } else {
-      $grid.isotope({
-        filter: '.project-item.' + skill
-      })
-    }
-  })
+    // Capture skill-filter click events
+    $('ul.menu li').on('click', function() {
+        var skill = $(this).attr('data-skill')
+        if (skill === 'Home') {
+            return
+        }
+        setActive(skill)
+        $.slidebars.close()
+        if (skill === 'All') {
+            $grid.isotope({
+                filter: '*'
+            })
+        } else {
+            $grid.isotope({
+                filter: '.project-item.' + skill
+            })
+        }
+    })
 
-  $('#page-loader').fadeOut(500)
+    $('#page-loader').fadeOut(500)
 }
 
-function checkAllImagesAreLoaded () {
-  var $img = $('img'),
-    totalImg = $img.length
+function checkAllImagesAreLoaded() {
+    var $img = $('img'),
+        totalImg = $img.length
 
-  var waitImgDone = function () {
-    totalImg--
+    var waitImgDone = function() {
+        totalImg--
 
-    // if all images are al
-    if (!totalImg) {
-      initializeGrid()
+        // if all images are al
+        if (!totalImg) {
+            initializeGrid()
+        }
     }
-  }
 
-  $('img').each(function () {
-    $(this)
-      .load(waitImgDone)
-      .error(waitImgDone)
-  })
+    $('img').each(function() {
+        $(this)
+            .load(waitImgDone)
+            .error(waitImgDone)
+    })
 }
--- portfolio/js/projects.js
+++ portfolio/js/projects.js
@@ -1,122 +1,155 @@
 this.projects = [{
-  name: 'Lisbon Swingers',
-  description: 'Big band Jazz Orchestra. <br> This responsive website required complex UI/UX components and subtle animations. <br>All done with plain and pure CSS, no framework.',
-  image: 'lisbonswingers.jpg',
-  url: {
-    preview: 'http://www.lisbonswingers.com/index_en.html',
-    github: 'https://github.com/rdquintas/LisbonSwingers',
-    simple: 'lisbonswingers.com',
-    liveSite: 'http://www.lisbonswingers.com/index_en.html'
-  },
-  skills: ['Javascript', 'LESS']
+    name: 'Lisbon Swingers',
+    description: 'Big band Jazz Orchestra. <br> This responsive website required complex UI/UX components and subtle animations. <br>All done with plain and pure CSS, no framework.',
+    image: 'lisbonswingers.jpg',
+    url: {
+        preview: 'http://www.lisbonswingers.com/index_en.html',
+        github: 'https://github.com/rdquintas/LisbonSwingers',
+        simple: 'lisbonswingers.com',
+        liveSite: 'http://www.lisbonswingers.com/index_en.html'
+    },
+    skills: ['Javascript', 'LESS']
 }, {
-  name: 'London Node Users Group',
-  description: 'I contribute and colaborate fixing open issues of LNUG website.',
-  image: 'lnug.jpg',
-  url: {
-    preview: 'http://www.lnug.org',
-    github: 'https://github.com/rdquintas/lnug.github.io',
-    simple: 'lnug.org',
-    liveSite: 'http://www.lnug.org'
-  },
-  skills: ['Javascript', 'NodeJS']
+    name: 'London Node Users Group',
+    description: 'I contribute and colaborate fixing open issues of LNUG website.',
+    image: 'lnug.jpg',
+    url: {
+        preview: 'http://www.lnug.org',
+        github: 'https://github.com/rdquintas/lnug.github.io',
+        simple: 'lnug.org',
+        liveSite: 'http://www.lnug.org'
+    },
+    skills: ['Javascript', 'NodeJS']
 }, {
-  name: 'Unplace Museum',
-  description: 'This project was for one of the biggest Modern Art institutions in Portugal (Fundação Calouste Gulbenkian).<br> The project was for a temporary international exhibition of world-wide modern artists. Site required inovative design and simple CMS support. <br>Extensive use of plain vanilla Javascript.',
-  image: 'unplace.jpg',
-  url: {
-    preview: 'http://www.ricardoquintas.com/portfolio/unplace/#%7B%22language%22%3A%22en%22%2C%22project_id%22%3Anull%2C%22tour_id%22%3Anull%7D',
-    github: 'https://github.com/rdquintas/unplace.org',
-    simple: 'unplace.org',
-    liveSite: 'http://www.unplace.org'
-  },
-  skills: ['Javascript', 'LESS']
+    name: 'Unplace Museum',
+    description: 'This project was for one of the biggest Modern Art institutions in Portugal (Fundação Calouste Gulbenkian).<br> The project was for a temporary international exhibition of world-wide modern artists. Site required inovative design and simple CMS support. <br>Extensive use of plain vanilla Javascript.',
+    image: 'unplace.jpg',
+    url: {
+        preview: 'http://www.ricardoquintas.com/portfolio/unplace/#%7B%22language%22%3A%22en%22%2C%22project_id%22%3Anull%2C%22tour_id%22%3Anull%7D',
+        github: 'https://github.com/rdquintas/unplace.org',
+        simple: 'unplace.org',
+        liveSite: 'http://www.unplace.org'
+    },
+    skills: ['Javascript', 'LESS']
 }, {
-  name: 'Santinho',
-  description: 'Proof of concept for a religious institution. <br>Features inovative UI/UX components.',
-  image: 'santinho.jpg',
-  url: {
-    preview: 'http://www.ricardoquintas.com/portfolio/santinho',
-    github: 'https://github.com/rdquintas/santinho',
-    simple: '',
-    liveSite: ''
-  },
-  skills: ['Javascript']
+    name: 'Angular Lab',
+    description: 'Stuff I did with Angular. <br> Here you can find plenty of exercises and practice stuff I did... hours, days and months of intense practice by me, are kept saved right here.',
+    image: 'angular.jpg',
+    url: {
+        preview: 'https://github.com/rdquintas/AngularLab',
+        github: 'https://github.com/rdquintas/AngularLab',
+        simple: '',
+        liveSite: 'https://github.com/rdquintas/AngularLab'
+    },
+    skills: ['Javascript', 'Angular']
 }, {
-  name: 'Alexandre Camarão',
-  description: 'Web portfolio of Portguese modern art artist Alexandre Camarão.<br> This website uses the flat file CMS framework: GRAV.',
-  image: 'alexandrecamarao.jpg',
-  url: {
-    preview: 'http://www.alexandrecamarao.com',
-    github: 'https://github.com/rdquintas/alexandrecamarao.com',
-    simple: 'alexandrecamarao.com',
-    liveSite: 'http://www.alexandrecamarao.com'
-  },
-  skills: ['Javascript', 'PHP', 'LESS']
+    name: 'Angular Shop',
+    description: 'This is a demo of a shopping website, totally developed using Angular. The app was deployed to Firebase <br> It\'s still a work in progress.',
+    image: 'angular-shop.jpg',
+    url: {
+        preview: 'https://ricardo-angular-shop.firebaseapp.com/',
+        github: 'https://github.com/rdquintas/ricardo-angular-shop',
+        simple: '',
+        liveSite: 'https://github.com/rdquintas/ricardo-angular-shop'
+    },
+    skills: ['Javascript', 'Angular']
 }, {
-  name: 'AFSO',
-  description: 'Website for Portguese ONG, AFSO. <br>This website uses the flat file CMS framework: GRAV.',
-  image: 'afso.jpg',
-  url: {
-    preview: 'http://www.ricardoquintas.com/portfolio/afso',
-    github: 'https://github.com/rdquintas/afso.pt',
-    simple: 'afso.pt',
-    liveSite: 'http://www.afso.pt'
-  },
-  skills: ['Javascript', 'PHP', 'LESS']
+    name: 'Santinho',
+    description: 'Proof of concept for a religious institution. <br>Features inovative UI/UX components.',
+    image: 'santinho.jpg',
+    url: {
+        preview: 'http://www.ricardoquintas.com/portfolio/santinho',
+        github: 'https://github.com/rdquintas/santinho',
+        simple: '',
+        liveSite: ''
+    },
+    skills: ['Javascript']
 }, {
-  name: 'Alphalink',
-  description: 'Civil Engineer company.<br> A simple website to display the company portfolio.<br> This website uses the flat file CMS framework: GRAV.',
-  image: 'alphalink.jpg',
-  url: {
-    preview: 'http://ricardoquintas.com/portfolio/alphalink/en',
-    github: 'https://github.com/rdquintas/Alphalink',
-    simple: 'alphalink.pt',
-    liveSite: 'http://www.alphalink.pt'
-  },
-  skills: ['Javascript', 'PHP', 'LESS']
+    name: 'Spring Lab',
+    description: 'Stuff I did with JAVA Spring. Plenty of code exercises and days of practice.',
+    image: 'spring.jpg',
+    url: {
+        preview: 'https://github.com/rdquintas/SpringLab',
+        github: 'https://github.com/rdquintas/SpringLab',
+        simple: '',
+        liveSite: 'https://github.com/rdquintas/SpringLab'
+    },
+    skills: ['JAVA']
 }, {
-  name: 'Ricardo Quintas',
-  description: 'My own website. <br>This responsive single-page website features some simple animations.',
-  image: 'ricardoquintas.jpg',
-  url: {
-    preview: 'http://ricardoquintas.com/',
-    github: 'https://github.com/rdquintas/ricardoquintas.com',
-    simple: 'ricardoquintas.com',
-    liveSite: 'http://www.ricardoquintas.com'
-  },
-  skills: ['Javascript', 'LESS', 'Bootstrap']
+    name: 'Alexandre Camarão',
+    description: 'Web portfolio of Portguese modern art artist Alexandre Camarão.<br> This website uses the flat file CMS framework: GRAV.',
+    image: 'alexandrecamarao.jpg',
+    url: {
+        preview: 'http://www.alexandrecamarao.com',
+        github: 'https://github.com/rdquintas/alexandrecamarao.com',
+        simple: 'alexandrecamarao.com',
+        liveSite: 'http://www.alexandrecamarao.com'
+    },
+    skills: ['Javascript', 'PHP', 'LESS']
 }, {
-  name: 'SCA',
-  description: "Proof of concept for SCA, a customer I've worked for. They requested me a POC for them to show to their stake-holders. <br>Very simple Bootstrap prototype.",
-  image: 'sca.jpg',
-  url: {
-    preview: 'http://ricardoquintas.com/portfolio/sca',
-    github: 'https://github.com/rdquintas/scademo',
-    simple: 'sca.com',
-    liveSite: 'http://www.sca.com/'
-  },
-  skills: ['Javascript', 'Bootstrap']
+    name: 'AFSO',
+    description: 'Website for Portguese ONG, AFSO. <br>This website uses the flat file CMS framework: GRAV.',
+    image: 'afso.jpg',
+    url: {
+        preview: 'http://www.ricardoquintas.com/portfolio/afso',
+        github: 'https://github.com/rdquintas/afso.pt',
+        simple: 'afso.pt',
+        liveSite: 'http://www.afso.pt'
+    },
+    skills: ['Javascript', 'PHP', 'LESS']
 }, {
-  name: 'Jazz Quotes',
-  description: 'This was a prototype I did for a design challenge.',
-  image: 'jazzquotes.jpg',
-  url: {
-    preview: 'http://codepen.io/rdquintas/full/RabBQr/',
-    github: 'http://codepen.io/rdquintas/pen/RabBQr/',
-    simple: '',
-    liveSite: 'http://codepen.io/rdquintas/full/RabBQr/'
-  },
-  skills: ['Javascript']
+    name: 'Alphalink',
+    description: 'Civil Engineer company.<br> A simple website to display the company portfolio.<br> This website uses the flat file CMS framework: GRAV.',
+    image: 'alphalink.jpg',
+    url: {
+        preview: 'http://ricardoquintas.com/portfolio/alphalink/en',
+        github: 'https://github.com/rdquintas/Alphalink',
+        simple: 'alphalink.pt',
+        liveSite: 'http://www.alphalink.pt'
+    },
+    skills: ['Javascript', 'PHP', 'LESS']
 }, {
-  name: 'Cristina Belo',
-  description: 'Website of yoga guru Cristina Belo.',
-  image: 'cristinabelo.jpg',
-  url: {
-    preview: 'http://www.cristinabelo.com',
-    github: 'https://github.com/rdquintas/cristinabelo.com',
-    simple: 'cristinabelo.com',
-    liveSite: 'http://www.cristinabelo.com'
-  },
-  skills: ['Javascript']
+    name: 'Ricardo Quintas',
+    description: 'My own website. <br>This responsive single-page website features some simple animations.',
+    image: 'ricardoquintas.jpg',
+    url: {
+        preview: 'http://ricardoquintas.com/',
+        github: 'https://github.com/rdquintas/ricardoquintas.com',
+        simple: 'ricardoquintas.com',
+        liveSite: 'http://www.ricardoquintas.com'
+    },
+    skills: ['Javascript', 'LESS', 'Bootstrap']
+}, {
+    name: 'SCA',
+    description: "Proof of concept for SCA, a customer I've worked for. They requested me a POC for them to show to their stake-holders. <br>Very simple Bootstrap prototype.",
+    image: 'sca.jpg',
+    url: {
+        preview: 'http://ricardoquintas.com/portfolio/sca',
+        github: 'https://github.com/rdquintas/scademo',
+        simple: 'sca.com',
+        liveSite: 'http://www.sca.com/'
+    },
+    skills: ['Javascript', 'Bootstrap']
+}, {
+    name: 'Jazz Quotes',
+    description: 'This was a prototype I did for a design challenge.',
+    image: 'jazzquotes.jpg',
+    url: {
+        preview: 'http://codepen.io/rdquintas/full/RabBQr/',
+        github: 'http://codepen.io/rdquintas/pen/RabBQr/',
+        simple: '',
+        liveSite: 'http://codepen.io/rdquintas/full/RabBQr/'
+    },
+    skills: ['Javascript']
+}, {
+    name: 'Cristina Belo',
+    description: 'Website of yoga guru Cristina Belo.',
+    image: 'cristinabelo.jpg',
+    url: {
+        preview: 'http://www.cristinabelo.com',
+        github: 'https://github.com/rdquintas/cristinabelo.com',
+        simple: 'cristinabelo.com',
+        liveSite: 'http://www.cristinabelo.com'
+    },
+    skills: ['Javascript']
 }]
